18686c5392ee3835125adae860d31a628865f4f8
Andy Kitchen <kitchen.andy+git@gmail.com>
2016-08-17 19:39:41 +1000

    changed initializer to match nn-detect, improved performance

--- src/ai/train.py
+++ src/ai/train.py
@@ -2,6 +2,7 @@ import os
 
 import numpy as np
 from numpy import newaxis
+import tensorflow as tf
 
 import tflearn
 from tflearn.layers.core import input_data, fully_connected
@@ -10,12 +11,16 @@ from tflearn.layers.estimator import regression
 
 import generate_data
 
+learning_rate = 0.001
+num_iterations = 1000
 
 def build_model():
+	init = tf.truncated_normal_initializer(stddev=1e-4)
+
 	network = input_data(shape=[None, 128, 128, 1], name='input')
-	network = conv_2d(network, nb_filter=2, filter_size=5, strides=1, activation='tanh')
-	network = fully_connected(network, 1, activation='linear')
-	network = regression(network, optimizer='adam', learning_rate=0.001,
+	network = conv_2d(network, nb_filter=2, filter_size=5, strides=2, activation='tanh', weights_init=init)
+	network = fully_connected(network, 1, activation='tanh', weights_init=init)
+	network = regression(network, optimizer='sgd', learning_rate=learning_rate,
 						 loss='mean_square', name='target')
 
 	model = tflearn.DNN(network, tensorboard_verbose=0, checkpoint_path='checkpoints/road_model1')
@@ -24,24 +29,27 @@ def build_model():
 
 if __name__ == '__main__':
 	input_size = 128
-	data_size = 2000
-
-	image_data, label_data = \
-		generate_data.generate_batch(
-			height=input_size,
-			width=input_size,
-			minibatch_size=data_size)
+	minibatch_size = 16
+	batch_size = 3200
 
 	checkpoint_path = 'checkpoints'
 	if not os.path.exists(checkpoint_path):
 		os.makedirs(checkpoint_path)
 
-	# Train
-
-	X = image_data
-	Y = label_data[:,newaxis]
-
 	model = build_model()
-	model.fit({'input': X}, {'target': Y}, n_epoch=15,
-			   validation_set=0.25,
-			   snapshot_step=100, show_metric=True, run_id='road_model1')
+
+	# Train
+	for i in range(num_iterations):
+		image_data, label_data = \
+			generate_data.generate_batch(
+				height=input_size,
+				width=input_size,
+				minibatch_size=batch_size)
+
+		X = image_data
+		Y = label_data[:,newaxis]
+
+		model.fit({'input': X}, {'target': Y},
+				   n_epoch=1,
+				   batch_size=minibatch_size,
+				   snapshot_epoch=True, show_metric=True, run_id='road_model1')
